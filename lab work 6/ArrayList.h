1107c94a4269ed42c1aa8dd7131e5c4918c5be77
anna <annahudz085@gmail.com>
2026-04-29 20:30:52 +0300

    ArrayList.h

--- lab work 6/ArrayList.h
+++ lab work 6/ArrayList.h
@@ -4,14 +4,14 @@
 
 class ArrayList {
 private:
-    int* data;
+    Point* data;
     int capacity;
     int size;
 
 
     void resize() {
         capacity = (capacity == 0) ? 1 : capacity * 2;
-        int* newData = new int[capacity];
+        Point* newData = new Point[capacity];
         for (int i = 0; i < size; i++) {
             newData[i] = data[i];
         }
@@ -24,7 +24,7 @@ public:
     ArrayList() : data(nullptr), capacity(0), size(0) {}
 
 
-    void add(int value) {
+    void add(Point value) {
         if (size == capacity) {
             resize();
         }
@@ -33,7 +33,7 @@ public:
 
 
 
-    bool remove(int value) {
+    bool remove(Point value) {
         for (int i = 0; i < size; i++) {
             if (data[i] == value) {
                 for (int j = i; j < size - 1; j++) {
@@ -47,29 +47,29 @@ public:
     }
 
 
-    bool contains(int value) const {
+    bool contains(Point value) const {
         for (int i = 0; i < size; i++) {
             if (data[i] == value) return true;
         }
         return false;
     }
 
-    void printRange(int min, int max) const {
+    void printRange(Point min, Point max) const {
         bool found = false;
         for (int i = 0; i < size; i++) {
-            if (data[i] >= min && data[i] <= max) {
+            if (!(data[i] <min) && !(max < data[i] )) {
                 std::cout << data[i] << " ";
                 found = true;
             }
         }
-        if (!found) std::cout << "Елементів не знайдено.";
+        if (!found) std::cout << "Elements not found.";
         std::cout << std::endl;
     }
 
 
     void display() const {
         if (size == 0) {
-            std::cout << "Список порожній." << std::endl;
+            std::cout << "List is empty." << std::endl;
             return;
         }
         for (int i = 0; i < size; i++) {
@@ -78,19 +78,22 @@ public:
         std::cout << " [size: " << size << ", cap: " << capacity << "]" << std::endl;
     }
 
-    void forEach(void (*action)(int&)) {
+    void forEach(void (*action)(Point&)) {
         for (int i = 0; i < size; i++) {
             action(data[i]);
         }
     }
 
 
-    void fillRandom(int count, int minVal = 0, int maxVal = 100) {
-        for (int i = 0; i < count; i++) {
-            add(minVal + rand() % (maxVal - minVal + 1));
-        }
+  void fillRandom(int n) {
+    for (int i = 0; i < n; i++) {
+        Point p;
+        p.x = (rand() % 1000) / 10.0; 
+        p.y = (rand() % 1000) / 10.0;
+        p.z = (rand() % 1000) / 10.0;
+        add(p); 
     }
-
+}
     ~ArrayList() {
         delete[] data;
     }
